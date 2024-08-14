d844f84113edaf456ef867a2e8742b3e69aeaca6
NazariiZmyslyi <nazarii.zmyslyi.knm.2020@lpnu.ua>
2024-08-14 14:24:55 +0300

    footer init

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,26 @@
 import Box from "@mui/material/Box";
-import {styled} from "@mui/material";
+import { styled, Typography } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TelegramIcon from "@mui/icons-material/Telegram";
 
 export const Footer = () => {
     return (
         <StyledBox>
-          dasgdfg
+            <Typography variant="h6" component="div">
+                StarWars Shop
+            </Typography>
+            <SocialIcons>
+                <a href="https://www.instagram.com/nazarzmyslyi/#" target="_blank" rel="noopener noreferrer">
+                    <InstagramIcon />
+                </a>
+                <a href="https://www.facebook.com/share/kGiryW7L9Cs8iTq5/?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer">
+                    <FacebookIcon />
+                </a>
+                <a href="https://t.me/BROOKISAME" target="_blank" rel="noopener noreferrer">
+                    <TelegramIcon />
+                </a>
+            </SocialIcons>
         </StyledBox>
     );
 };
@@ -13,6 +29,30 @@ const StyledBox = styled(Box)`
     bottom: 0;
     position: absolute;
     width: 100%;
-    height: 40px;
-    background: green;
+    height: 60px;
+    background: black;
+    color: yellow;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    font-family: 'Star Jedi', sans-serif;
+`;
+
+const SocialIcons = styled(Box)`
+    display: flex;
+    gap: 15px;
+
+    a {
+        color: yellow;
+        transition: color 0.3s ease-in-out;
+    }
+
+    a:hover {
+        color: white;
+    }
+
+    svg {
+        font-size: 28px;
+    }
 `;
